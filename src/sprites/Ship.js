1dcf6bcdd375c83bfa03dfb96849c0da1c8c0a4e
Christian Tobias Schmitt <ctt@Christians-MacBook-Pro.local>
2017-02-14 21:54:45 +0100

    Ship movement

--- src/sprites/Ship.js
+++ src/sprites/Ship.js
@@ -2,13 +2,112 @@ import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
 
-  constructor ({ game, x, y, asset }) {
-    super(game, x, y, asset)
+  constructor ({ game, x, y, asset, assetpath, level }) {
+    super(game, x, y, asset, assetpath)
+
+    this.level = level;
     this.anchor.setTo(0.5)
+    // this.angle = 2;
+
+    this.MOVE_SPEED = 150;
+    // this.scale.set(0.5);
+    this.animations.add('idle', Phaser.Animation.generateFrameNames('Ship_1000', 3, 3, '', 0), 5, true, false);
+    this.animations.add('move', Phaser.Animation.generateFrameNames('Ship_1000', 1, 13, '', 0), 10, true, false);
+    this.play('idle');
+
+    this.game.physics.arcade.enable(this);
+    // this.body.allowRotation = false;
+
+    this.body.setSize(40, 20, 15, 17);
+
+    this.initKeyboard();
+
+    // this.game.input.onDown.add(this.move, this);
+    this.game.camera.follow(this);
+
+    this.reset();
   }
 
+  initKeyboard() {
+   this.keyboardCursors = this.game.input.keyboard.createCursorKeys();
+   this.moveSpeed = { x: 0, y: 0 }
+
+   this.wasd = {
+     up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
+     down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
+     left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
+     right: this.game.input.keyboard.addKey(Phaser.Keyboard.D),
+   };
+ }
+
   update () {
-    this.angle += 1
+    // this.angle += 1
+    this.updatePlayer()
+
+    this.game.physics.arcade.collide(this, this.level.collisionLayer)
+  }
+
+  move (pointer) {
+    if (this.tween && this.tween.isRunning) {
+        this.tween.stop();
+      }
+    this.rotation = this.game.physics.arcade.angleToPointer(this, pointer);    //  300 = 300 pixels per second = the speed the sprite will move at, regardless of the distance it has to travel
+    var duration = (this.game.physics.arcade.distanceToPointer(this, pointer) / 300) * 1000;
+    this. tween = this.game.add.tween(this).to({ x: pointer.x, y: pointer.y }, duration, Phaser.Easing.Linear.None, true);
+  }
+
+  updatePlayer() {
+    // shorthand so i don't have to reference this all the time
+    let keyboardCursors = this.keyboardCursors;
+    let wasd = this.wasd;
+    let player = this;
+    let moveSpeed = this.moveSpeed;
+    let joystick = this.joystick;
+
+    // set our player's velocity to 0
+    // so the sprite doesn't move when there is no input from our player
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+
+    // keyboard movement
+    // left and right keyboard movement
+    if (keyboardCursors.left.isDown || wasd.left.isDown) moveSpeed.x = -player.MOVE_SPEED;
+    else if (keyboardCursors.right.isDown || wasd.right.isDown) moveSpeed.x = player.MOVE_SPEED;
+    else moveSpeed.x = 0;
+
+    // up and down keyboard movement
+    if (keyboardCursors.up.isDown || wasd.up.isDown) moveSpeed.y = -player.MOVE_SPEED;
+    else if (keyboardCursors.down.isDown || wasd.down.isDown) moveSpeed.y = player.MOVE_SPEED;
+    else moveSpeed.y = 0;
+
+    if(Math.abs(moveSpeed.x) > 0 || Math.abs(moveSpeed.y) > 0) {
+      player.body.velocity.x = moveSpeed.x;
+      player.body.velocity.y = moveSpeed.y;
+
+      // set direction using Math.atan2
+      let targetPos = { x: player.x + moveSpeed.x, y: player.y + moveSpeed.y };
+      player.rotation = Math.atan2(targetPos.y - player.y, targetPos.x - player.x);
+    }
+
+    if (game.input.activePointer.isDown) {
+      this.rotation = game.physics.arcade.moveToPointer(this, this.MOVE_SPEED, game.input.activePointer, 800);
+    }
+
+
+    // check if player is moving
+    if(Math.abs(player.body.velocity.x) > 0 || Math.abs(player.body.velocity.y) > 0) {
+      // play the animation, phaser just returns when it's currently animating
+      // so it's fine to call it on every frame
+      player.play('move');
+    } else {
+      player.play('idle');
+    }
+  }
+
+  reset() {
+    let player1Start = this.level.map.objects.Meta.find( o => o.name == 'Player1Start');
+    this.position.set(player1Start.x, player1Start.y);
+    this.angle = 0;
   }
 
 }
--- src/states/Game.js
+++ src/states/Game.js
@@ -2,6 +2,7 @@
 import Phaser from 'phaser'
 import GameAbstract from './GameAbstract'
 import Level2 from '../maps/Level2'
+import Ship from '../sprites/Ship'
 
 export default class extends GameAbstract {
   init () {}
@@ -9,41 +10,33 @@ export default class extends GameAbstract {
     super.preload();
     game.load.tilemap('level2', '/assets/tilemap/level2.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('tiles', '/assets/tilemap/tileset.png');
-    game.load.image('tiles', '/assets/tilemap/tileset.png');
+    game.load.atlasJSONHash('ship1', '/assets/sprites/ship-1.png', '/assets/sprites/ship-1.json');
+    // game.load.image('tiles', '/assets/tilemap/tileset.png');
   }
 
   create () {
     super.create();
 
-    Level2.initTilemap();
+    this.level2 = new Level2()
+    this.level2.initTilemap();
+
+    this.ship = new Ship({
+      game: this.game,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'ship1',
+      assetpath: 'Ship_10001',
+      level: this.level2
+    })
+
+    this.game.add.existing(this.ship)
 
   }
 
-  // initTilemap() {
-  //   let map = game.add.tilemap('level2');
-  //   this.map = map;
-  //
-  //   map.addTilesetImage('tiles', 'tiles');
-  //   map.createLayer('Base');
-  //
-  //   let collisionLayer = map.createLayer('Collision');
-  //   this.collisionLayer = collisionLayer;
-  //
-  //   collisionLayer.visible = false;
-  //
-  //   map.setCollisionByExclusion([], true, this.collisionLayer);
-  //
-  //   collisionLayer.resizeWorld();
-  //
-  //   // this.initPlayer();
-  //
-  //   // let exit = this.map.objects.Meta.find( o => o.name == 'exit');
-  //   // this.exitRect = new Phaser.Rectangle(exit.x, exit.y, exit.width, exit.height);
-  // }
-
   render () {
     if (__DEV__) {
-      // this.game.debug.spriteInfo(this.mushroom, 32, 32)
+      this.game.debug.body(this.ship)
+      this.game.debug.spriteInfo(this.ship, 32, 32)
     }
   }
 }
